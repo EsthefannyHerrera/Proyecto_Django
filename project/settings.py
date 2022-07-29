3aec490115ca4837bcc572a54c8ed31102f831b0
EsthefannyHerrera <esthefanny.herrera@outlook.com>
2022-07-28 21:03:25 -0500

    build: configure whitenoise

--- project/settings.py
+++ project/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/static/'
+STATIC_ROOT ='static'
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
